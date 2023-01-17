fad255af9306a41ae1e24d64bcc9a0daecf1fe84
GentleMizt <olamideiyanda18@gmail.com>
2023-01-17 15:44:42 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const name = 'lancer';
+  const age = 26;  
+
+  const handleChange = () =>{
+    setUserName('Lancer');
+  }
+  const [userName, setUserName] = useState('Rider');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello {name} </h1>
+      {age >= 18 ? <p>You're Old enough</p> : <p>You are a young person</p>}
+      <p>My name is {name} and i'm {age} years old.</p>
+      <p>The default username is {userName}</p>
+      
+      <button onClick={handleChange}>Click me to change name</button>
+      {/* <h2></h2> */}
     </div>
   );
 }
 
 export default App;
+
+
+// A react website can only refresh if the state or the prop changes
+
+// Conditionals
+// States - 
+// Props
+// Class
+// Events
+// List 
+// forms
+
+
+// Hooks
+// 1. useState - saves a state of a current object, it is also used to create a state.
+// 2. useContext
+// 3. useEffect - to fetch data like APIs 
+// 4. useRef
+// 5. useCallback
+// 6. useMemo
+
+// whenever u want to use a hook in react, u have to bring it into react by importing the hook 
+// whenever we create a state, we have a variable that holds the state value, and we have a function that changes the variable value.
